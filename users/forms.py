423b62ff46b0252f3bc563d3516f1cf0d0c12e5a
Sagar Lama <sagarlama826@gmail.com>
2019-02-08 08:54:32 +0545

    profile update working

--- users/forms.py
+++ users/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+from users.models import Profile
 from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
@@ -8,3 +9,14 @@ class UserRegisterForm(UserCreationForm):
     class Meta():
         model = User
         fields = ['username','email','password1','password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta():
+        model = User
+        fields = ['username','email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
