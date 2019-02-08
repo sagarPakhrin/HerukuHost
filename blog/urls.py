357b931174db5391e0675664fbfcab3de817dc6c
Sagar Lama <sagarlama826@gmail.com>
2019-02-08 09:20:59 +0545

    PostListView working

--- blog/urls.py
+++ blog/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path,include
 from blog import views
 urlpatterns = [
-    path('', views.home,name='blog-home'),
+    path('', views.PostListView.as_view(template_name='blog/home.html'),name='blog-home'),
     path('about/', views.about,name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import (ListView)
 from blog.models import Post
 # Create your views here.
 
-def home(request):
-    return render(request,'blog/home.html')
+class PostListView(ListView):
+    model = Post
+    template_name='blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
 def about(request):
     return render(request,'blog/about.html')
