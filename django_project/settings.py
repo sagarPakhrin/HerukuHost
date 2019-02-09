56054946f7e9acdf1563420330d33c2665920997
Sagar Lama <sagarlama826@gmail.com>
2019-02-09 15:39:03 +0545

    Finished

--- django_project/settings.py
+++ django_project/settings.py
@@ -132,3 +132,12 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sagarlama826@gmail.com'
+EMAIL_HOST_PASSWORD = 'Psh#4$8585mC'
